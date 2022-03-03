7af590c036c803bae756e0375baa680f7c68c9ea
vinihal <vinifromthefuture@gmail.com>
2022-03-02 21:18:43 -0300

    Criação de classe e objeto

---
+++ conta.py
@@ -0,0 +1,6 @@
+class Conta:
+    def __init__(self, numero, titular, saldo, limite):
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
--- ola.py
+++ ola.py
@@ -1,1 +1,9 @@
-print('Ola mundo')
+def cria_conta(numero, nome, saldo, limite):
+    conta = {"numero": numero, "titular": nome, "saldo": saldo, "limite": limite}
+    return conta
+def deposita(conta, valor):
+    conta["saldo"] += valor
+def saca(conta, valor):
+    conta["saldo"] -= valor
+def extrato(conta):
+    print("O seu saldo é {}".format(conta["saldo"]))
